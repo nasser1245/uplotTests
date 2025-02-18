6670ffa71af22fecf8f42c1b606caac4af72d0e0
nasser1245 <nasser1245@gmail.com>
2025-02-18 14:05:07 +0330

    some visual improvements

--- src/components/UPlotChart.vue
+++ src/components/UPlotChart.vue
@@ -2,11 +2,20 @@
 import { onMounted, ref, watch, onBeforeUnmount } from 'vue'
 import uPlot from 'uplot'
 import 'uplot/dist/uPlot.min.css'
-
+//import signalR from '@microsoft/signalr'
 const chartContainer = ref(null)
 let chartInstance = null
 const numSeries = 40
 const numPoints = 3600
+// const connection = new signalR.HubConnectionBuilder().withUrl('/chat').build()
+
+// connection.on('send', (data) => {
+//   console.log(data)
+// })
+// const sendData = () => {
+//   console.log('data sent')
+//   connection.start().then(() => connection.invoke('send', 'Solton'))
+// }
 
 const generateSmoothData = (series = 2, points = 50) => {
   const data = new Array(series).fill(null).map(() => new Array(points))
@@ -17,7 +26,24 @@ const generateSmoothData = (series = 2, points = 50) => {
     options[j] = {
       label: `دمای ${j + 1}`,
       stroke: '#' + (((1 << 24) * Math.random()) | 0).toString(16).padStart(6, '0'),
-      width: 2,
+      width: 1,
+      dash: [100, 10, 50, 20],
+
+      value: (self, rawValue) => {
+        try {
+          const minInRange = self.series[j + 1].min.toFixed(2)
+          const maxInRange = self.series[j + 1].max.toFixed(2)
+          return `
+          • ${rawValue.toFixed(2)}\r\n
+          ↧ ${minInRange}\r\n
+          ↥ ${maxInRange}\r\n
+          ↧ ${minInRange}\r\n
+          ↥ ${maxInRange}\r\n
+          `
+        } catch (e) {
+          return '--'
+        }
+      },
     }
     data[j][0] = start
     for (let i = 1; i < points; i++) {
@@ -34,8 +60,8 @@ const xAxis = Array.from({ length: numPoints }, (_, i) => i)
 const ySeries = generateSmoothData(numSeries, numPoints)
 const chartData = ref([xAxis, ...ySeries.data])
 
-const isDragging = ref(false)
-const startX = ref(0)
+//const isDragging = ref(false)
+//const startX = ref(0)
 const options = {
   title: `${numSeries} Series Chart with X [0-${numPoints - 1}]`,
   width: 1000,
@@ -44,7 +70,12 @@ const options = {
     x: { time: false, min: 0, max: numPoints - 1 },
     y: { auto: true },
   },
-  series: [{ label: 'X Axis' }, ...ySeries.options],
+  series: [{ label: 'زمان' }, ...ySeries.options],
+  // cursor: {
+  //   drag: {
+  //     setScale: false, // ✅ Disables default box zoom
+  //   },
+  // },
   // axes: [
   //   { space: 40 }, // X-axis spacing
   //   { space: 50 }, // Y-axis spacing
@@ -56,7 +87,6 @@ const options = {
           e.preventDefault() // Prevent page scrolling
           let xMin = u.scales.x.min
           let xMax = u.scales.x.max
-          if (xMin < 0 && xMax > numPoints && e.deltaY > 0) return
           console.log('Mouse wheel event')
           const scale = e.deltaY > 0 ? 1.2 : 0.8
 
@@ -75,65 +105,33 @@ const options = {
         //let isDragging = false
         //let startX = 0
 
-        chartContainer.value.addEventListener('mousedown', (e) => {
-          isDragging.value = true
-          startX.value = e.clientX
-        })
+        // chartContainer.value.addEventListener('mousedown', (e) => {
+        //   isDragging = true
+        //   startX = e.clientX
+        // })
 
         chartContainer.value.addEventListener('mousemove', (e) => {
-          if (!isDragging.value) return
+          console.log(Math.round(chartInstance.posToVal(e.offsetX, 'x')))
+          // if (!isDragging) return
 
-          const deltaX = e.clientX - startX.value
-          const shift = (deltaX * (xMax - xMin)) / u.width
+          // const deltaX = e.clientX - startX
+          // const shift = (deltaX * (xMax - xMin)) / u.width
+          // xMin -= shift
+          // xMax -= shift
+          // startX = e.clientX
 
-          xMin -= shift
-          xMax -= shift
-          startX.value = e.clientX
-
-          u.setScale('x', { min: xMin, max: xMax })
+          // u.setScale('x', { min: xMin, max: xMax })
         })
 
-        chartContainer.value.addEventListener('mouseup', () => (isDragging.value = false))
-        chartContainer.value.addEventListener('mouseleave', () => (isDragging.value = false))
-
-        //let isDragging = false
-        //let startX = 0
-        // chartContainer.value.addEventListener('mousedown', (e) => {
-        //   e.preventDefault()
-        //   isDragging.value = true
-        //   startX.value = e.clientX
-        // })
-        // chartContainer.value.addEventListener('mousemove', (e) => {
-        //   e.preventDefault()
-
-        //   if (!isDragging.value) return
-
-        //   const deltaX = e.clientX - startX.value
-        //   const shift = (deltaX * (xMax - xMin)) / 600
-        //   xMin -= shift
-        //   xMax -= shift
-        //   startX.value = e.clientX
-
-        //   u.setScale('x', { min: xMin, max: xMax })
-        // })
-
-        // chartContainer.value.addEventListener('mouseup', () => {
-        //   e.preventDefault()
-
-        //   isDragging.value = false
-        // })
-
-        // chartContainer.value.addEventListener('mouseleave', () => {
-        //   e.preventDefault()
-
-        //   isDragging.value = false
-        // })
+        // chartContainer.value.addEventListener('mouseup', () => (isDragging = false))
+        // chartContainer.value.addEventListener('mouseleave', () => (isDragging = false))
       },
     ],
   },
 }
 
-onMounted(() => {
+onMounted(async () => {
+  //await sendData()
   if (chartContainer.value) {
     chartInstance = new uPlot(options, chartData.value, chartContainer.value)
   }
@@ -166,5 +164,15 @@ const resetZoom = () => {
 <template>
   <button @click="resetZoom" :key="chartKey">Reset Zoom</button>
   <div ref="chartContainer"></div>
-  <div style="height: 500px">test</div>
 </template>
+
+<style>
+#app > div > main > div:nth-child(2) > div > table > tbody > tr > th > div.u-label {
+  color: red;
+}
+
+#app > div > main > div:nth-child(2) > div > table > tbody > tr > td.u-value {
+  font-size: 10pt;
+  word-wrap: break-word;
+}
+</style>
